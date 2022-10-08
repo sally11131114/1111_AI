da1f3cbee1796b61f4e653196e294655abcda665
sally11131114 <sally11131114@gmail.com>
2022-10-08 23:14:48 +0800

    hw2 done==

--- hw2/BF.py
+++ hw2/BF.py
@@ -1,5 +1,8 @@
 import json
 import math
+from unittest import result
+
+from matplotlib.pyplot import flag
 class Problem:
     def __init__(self, input):
         self.input = input
@@ -9,24 +12,38 @@ class Problem:
         for task, agent in enumerate(ans):
             totalTime += self.input[task][agent]
         return totalTime
-
-def permutations(input, end=[], result=[]):
+result=[]
+def permutations(input, num, end=[]):
+    global result
     in_len=len(input)
+    # if flag==1:
+    #     result=[]
+    #     flag=0
     if in_len == 0:
         result.append(list(end))
+        #print('result:', result)
+        # if len(result) == math.factorial(num):
+        #     print('len(result): ', len(result))
+        #     print('math.factorial(global_num)', math.factorial(num))
+        #     print('result: ', result)
+        #     return result
     else:
         for i in range(in_len):
-            permutations(input[:i] + input[i+1:], end + input[i:i+1])
-    return result
+            permutations(input = input[:i] + input[i+1:], num=num, end= end + input[i:i+1])
 
 def BF(solver):
-    print('numAgents:', solver.numTasks)
-    result = permutations(list(range(solver.numTasks)))
+    global result
+    #print('numAgents:', solver.numTasks)
+    # global_num = solver.numTasks
+    # print('global_num: ', global_num)
+    #result=[]
+    permutations(list(range(solver.numTasks)), num=solver.numTasks)
+    #print('####', result)
 
     # if result>math.factorial(solver.numTasks):
     #     result = result[]
-    if solver.numTasks==4:
-        print(result)
+    # if solver.numTasks==4:
+    #     print(result)
     min = solver.cost(result[0])
     index = 0
     for i in range(len(result)):
@@ -60,3 +77,4 @@ if __name__ == '__main__':
             BFans = BF(solver)
             print('Assignment:', BFans) # print 出分配結果
             print('Cost:', solver.cost(BFans)) # print 出 cost 是多少
+            result.clear()
